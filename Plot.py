4e0ffdd418d538c68be9abfb9b42e03762b4d144
Bakinam Tarik Essawy <btaessawy@gmail.com>
2019-08-02 11:51:33 -0400

    Add files via upload

--- Plot.py
+++ Plot.py
@@ -1,4 +1,4 @@
-# first, list all files that includes specific starting and ending dates
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
@@ -12,16 +12,9 @@ import dask
 
 current_dir = os.getcwd()
 
-# Print path to make sure it is the correct working directory path
-
-#print(current_dir)
-
 # Use the cleaned data from the current working directory
 list = glob(os.path.join(current_dir, "clean_data/*"))
 
-#data = pd.Series(os.path.join(current_dir, "clean_data"))
-#for file in glob(os.path.join(current_dir, "clean_data/*")):
-    # print(file)
 
 ds1 = xr.open_mfdataset(list)
 comid = 22340547
@@ -30,21 +23,12 @@ ds2 = ds1.sel(feature_id=comid)['streamflow']
 
 df = ds2.to_dataframe()
 
-#ax = df.plot(figsize=(20,8), title = 'Reanalysis Stream Flow for ComID = 229757')
-#ax.set(xlabel = 'Date', ylabel = 'StreamFlow(cms)');
-
 plt.figure()
 df.streamflow.plot(style='k--', label='Series')
-#plt.legend(); plt.legend(loc='best')
+#plt.legend(); plt.legend(loc='best') # In case uf you need to add a legend
 plt.xlabel('Days')
 plt.ylabel('stream flow (m3/sec)')
 plt.title('The Stream Flow Time Series for ComID 229757')
 plt.grid(True)
 plt.savefig("comid_22340547.png")
 plt.show()
-
-#df.plot(kind='line', x='time', y='Stream Flow', ax=ax)
-
-
-#Data_frame.streamflow.plot()
-#plot.show()
