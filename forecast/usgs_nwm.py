c441e5be99a51f463f6df5ce45c829aa8f5eaad4
slawler <slawler@gmu.edu>
2019-08-05 09:53:10 -0400

    add available data function, add  edge cases in nb

--- forecast/usgs_nwm.py
+++ forecast/usgs_nwm.py
@@ -3,7 +3,57 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def usgs_descrip_to_dict(descrip: list, gage: str):
+
+def get_gage_meta(gage: str) -> dict:
+    """
+    Scrape metadata from USGS website
+    :nested functions:
+        1. usgs_descrip_to_dict
+    :param gage: usgs gage ID(e.g. '05418500')
+    :return: selected metadata
+    """
+    url = f'https://waterdata.usgs.gov/nwis/inventory/?site_no={gage}&agency_cd=USGS'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    for row in soup.find('div', {"id": "stationTable"}).contents:
+        if 'Datum' in str(row):
+            break
+    descrip = [j.text.replace('\xa0','').replace('  ', ' ') for j in row.find_all('dd')]
+    return usgs_descrip_to_dict(descrip, gage)
+
+def get_gage_available_data(gage: str) -> pd.DataFrame:
+    """
+    Scrape available data table from USGS website
+    :param gage: usgs gage ID(e.g. '05418500')
+    :return: html table as dataframe
+    """
+    url = f'https://waterdata.usgs.gov/nwis/inventory/?site_no={gage}&agency_cd=USGS'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'lxml')
+    table = soup.find_all('table')[0]
+    return pd.read_html(str(table))[0]
+
+def deg_to_dec(string):
+    """
+    converts a coordinate in degrees, converts to decimal degree
+    :param string:
+    :return:
+    """
+    deg = float(string.split('°')[0])
+    mn = float(string.split('\'')[0].split('°')[1])
+    sec = float(string.split('\'')[1].replace('"', ''))
+    dd = deg + mn/60 + sec/3600
+    return dd
+
+def usgs_descrip_to_dict(descrip: list, gage: str) -> dict:
+    """
+    Takes a list of strings from the USGS Website, and turns it into a dictionary of metadata
+    :nested functions:
+        1. deg_to_dec
+    :param descrip: container for data from metadata ()
+    :param gage: Gauge ID (e.g. '05418500')
+    :return:
+    """
     d = {}
     for x in descrip:
         if 'Lat' in x:
@@ -11,7 +61,7 @@ def usgs_descrip_to_dict(descrip: list, gage: str):
             d['lon'] = deg_to_dec(x.split(', ')[1].split(' ')[1])
             d['horizontal datum'] = x.split(' ')[-1]
         elif 'Hydrologic' in x:
-            d['HUC8'] = int(x.split(' ')[-1])
+            d['HUC8'] = x.split(' ')[-1]
         elif 'Datum' in x:
             d['vertical datum'] = x.split('above ')[1].replace('.', '')
             d['feet above vertical datum'] = float(x.split(': ')[1].split(' feet')[0].replace(',', ''))
@@ -20,34 +70,31 @@ def usgs_descrip_to_dict(descrip: list, gage: str):
     d['gage'] = gage
     return d
 
-def deg_to_dec(string):
-    deg = float(string.split('°')[0])
-    mn = float(string.split('\'')[0].split('°')[1])
-    sec = float(string.split('\'')[1].replace('"', ''))
-    dd = deg + mn/60 + sec/3600
-    return dd
 
-def get_gage_meta(gage: str):
-    url = f'https://waterdata.usgs.gov/nwis/inventory/?site_no={gage}&agency_cd=USGS'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    for i in soup.find('div', {"id": "stationTable"}).contents:
-        if 'Datum' in str(i):
-            break
-    descrip = [j.text.replace('\xa0','').replace('  ', ' ') for j in i.find_all('dd')]
-    return usgs_descrip_to_dict(descrip, gage)
-
-def new_plot(figsize=(20,6), fontsize=18) -> plt.subplots:
-    '''Return a created new plotframe.'''
-    
+def new_plot(figsize:tuple = (20, 6), fontsize: int = 18,
+             xlabel:str = 'Discharge (cms)', ylabel:str = 'Stage') -> plt.subplots:
+    """
+    Generic plot setup
+    :param figsize: recommeded 20,6 for notebooks
+    :param fontsize: label fontsize
+    :param xlabel: be careful, best to make a test to verify units are consistent
+    :param ylabel: be careful, best to make a test to verify units are consistent
+    :return: matplotlib fig, ax
+    """
     fig,  ax = plt.subplots(figsize=figsize)
-    ax.set_xlabel('Discharge (cms)', fontsize=fontsize)
-    ax.set_ylabel('Stage', fontsize=fontsize)
+    ax.set_xlabel(xlabel, fontsize=fontsize)
+    ax.set_ylabel(ylabel, fontsize=fontsize)
+    # labelsize is the ticsize
     ax.tick_params(axis='both', which='major', labelsize=18)
     ax.grid(which='major', color='lightgrey', linestyle='--', linewidth=2)
     return fig, ax
 
 def get_USGS_ratingcurve(gage: str):
+    """
+    Reads the table beginning after comment lines at url
+    :param gage:
+    :return:
+    """
     url = f'https://waterdata.usgs.gov/nwisweb/get_ratings?site_no={gage}&file_type=exsa'
     r = requests.get(url)
     a = r.text.split('\n')
