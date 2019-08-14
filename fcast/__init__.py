700056d902fe3c77edaeccd885bea05cefa767b3
albrazeau <abrazeau1993@gmail.com>
2019-08-14 09:07:58 -0400

    add version to __init__.py

--- fcast/__init__.py
+++ fcast/__init__.py
@@ -2,3 +2,5 @@ from .GageUSGS import GageUSGS
 from .NHDPlusStreamSegment import StreamSegmentNHD
 from .plotting import *
 from .NWM import ShortRange, MediumRange, Assim, get_USGS_rc, get_USGS_stations
+
+__version__ = "0.0.1"
