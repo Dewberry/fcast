8e8e4449c13056675a102607486cb19468c0623e
albrazeau <abrazeau1993@gmail.com>
2019-08-14 09:25:38 -0400

    add setup.py and fix version

--- fcast/__init__.py
+++ fcast/__init__.py
@@ -3,4 +3,4 @@ from .NHDPlusStreamSegment import StreamSegmentNHD
 from .plotting import *
 from .NWM import ShortRange, MediumRange, Assim, get_USGS_rc, get_USGS_stations
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
