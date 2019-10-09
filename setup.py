f58b5c83ef6cba2a48c9f965cca9f5bb6125ea36
albrazeau <abrazeau1993@gmail.com>
2019-10-09 15:18:16 -0400

    update license in setup, upload to pypi

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author_email="abrazeau@dewberry.com",
     license="Apache License 2.0",
     classifiers=[
-        "License :: OSI Approved :: Apache License 2.0",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
